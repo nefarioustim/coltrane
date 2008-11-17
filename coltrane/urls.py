90cbe5f16bc8a2661d73d8c31364b173de9a6c57
Tim Huegdon <tim@nefariousdesigns.co.uk>
2008-11-17 10:34:30 +0000

    testing indents

--- coltrane/urls.py
+++ coltrane/urls.py
@@ -6,17 +6,19 @@ from django.contrib import admin
 admin.autodiscover()
 
 from coltrane.feeds import CategoryFeed, LatestEntriesFeed
-feeds = { 'entries': LatestEntriesFeed,
- 		  'categories': CategoryFeed }
+feeds = {
+    'entries': LatestEntriesFeed,
+    'categories': CategoryFeed
+}
 
 urlpatterns = patterns('',
     (r'^admin/(.*)', admin.site.root),
-	(r'^comments/', include('django.contrib.comments.urls')),
-	(r'^categories/', include('coltrane.urls.categories')),
-	(r'^links/', include('coltrane.urls.links')),
-	(r'^tags/', include('coltrane.urls.tags')),
-	(r'^', include('coltrane.urls.entries')),
-	(r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', { 'feed_dict': feeds }),
+    (r'^comments/', include('django.contrib.comments.urls')),
+    (r'^categories/', include('coltrane.urls.categories')),
+    (r'^links/', include('coltrane.urls.links')),
+    (r'^tags/', include('coltrane.urls.tags')),
+    (r'^', include('coltrane.urls.entries')),
+    (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', { 'feed_dict': feeds }),
 )
 
 # this is for serving static files in development
@@ -29,11 +31,11 @@ if settings.DEBUG:
                 'document_root': os.path.join(os.path.dirname(__file__), '../static')
             }
         ),
-		(
-			r'^yui/(?P<path>.*)$',
-			'django.views.static.serve',
-			{
-				'document_root': '/Users/Shared/Sites/yui/build'
-			}
-		),
+        (
+            r'^yui/(?P<path>.*)$',
+            'django.views.static.serve',
+            {
+                'document_root': '/Users/Shared/Sites/yui/build'
+            }
+        ),
     )
